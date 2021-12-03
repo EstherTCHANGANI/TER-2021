6bad6241af6e7f45864eea9b041ae66b380e0751
Gomes Silva Filipe <44194360+Leankh@users.noreply.github.com>
2021-12-03 11:46:33 +0100

    Ajout des différents checkbox clusters #33
    - Ajout des checkboxs clusters
    
    - Création des components de recherche
    
    - Placement du search filters dans la page de recherche

--- Front-end/CROBORA-App/src/app/app.module.ts
+++ Front-end/CROBORA-App/src/app/app.module.ts
@@ -1,22 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { MatSelectModule } from '@angular/material/select'; 
+import { MatSelectModule } from '@angular/material/select';
+import { MatCheckboxModule } from '@angular/material/checkbox';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchFiltersComponent } from './search-filters/search-filters.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SearchPageComponent } from './search-page/search-page.component';
+import { ListViewComponent } from './list-view/list-view.component';
+import { ClusterViewComponent } from './cluster-view/cluster-view.component';
+import { TreemapsComponent } from './treemaps/treemaps.component';
+import { DistributionComponent } from './distribution/distribution.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchFiltersComponent
+    SearchFiltersComponent,
+    SearchPageComponent,
+    ListViewComponent,
+    ClusterViewComponent,
+    TreemapsComponent,
+    DistributionComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSelectModule
+    MatSelectModule,
+    MatCheckboxModule
   ],
   providers: [],
   bootstrap: [AppComponent]
