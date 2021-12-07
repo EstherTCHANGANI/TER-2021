7a30260200cf92d1ab5219130de796f574b6040c
youssraibrahimi12 <92360032+youssraibrahimi12@users.noreply.github.com>
2021-12-07 23:53:00 +0100

    Update CONSTANTES.py

--- TER-base-donnée/import/CONSTANTES.py
+++ TER-base-donnée/import/CONSTANTES.py
@@ -2,5 +2,6 @@
 PATH_META = "Data/MetaDonnees/*.xlsx"
 PATH_FICHES = "Data/Fiches/*.xlsx"
 UNNAMED = "Unnamed"
-
+file_name = "source/fiche/MetaDonn.csv"
+fiche_name = "source/fiche/fiches_INA.csv"
 COLONNES = []
