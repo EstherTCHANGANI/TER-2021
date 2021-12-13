e64f425bd4e4d3578092870d6f794aa5e1c393ee
youssraibrahimi12 <92360032+youssraibrahimi12@users.noreply.github.com>
2021-12-13 14:37:15 +0100

    Update LoadFile.py

--- TER-base-donnée/import/LoadFile.py
+++ TER-base-donnée/import/LoadFile.py
@@ -2,21 +2,8 @@ import glob
 import json
 import pandas as pd
 import os
-import argparse
-from openpyxl import load_workbook
 from pymongo import MongoClient
-
-parser = argparse.ArgumentParser(description='Load data into mongoDB collection')
-parser.add_argument('dataPath', metavar='dataPath', type=str,
-                    help='The path of the directory containing the data')
-parser.add_argument('--outputPath', type=str, default="", nargs="?",
-                    help='The path of the directory where the CSV files will be stored')
-parser.add_argument("--mongoUrl", type=str, default="mongodb://localhost:27017",
-                    help="URL of the mongoDB server")
-parser.add_argument("--mongoDatabase", type=str, default="admin",
-                    help="Name of the mongoDB data base")
-parser.add_argument("--clear", type=bool, default=False,
-                    help="Clear CSV files after run")
+from CONSTANTES import *
 
 
 def import_csv_mongo(db_url, db_name, csv_file_path):
@@ -34,18 +21,4 @@ def import_csv_mongo(db_url, db_name, csv_file_path):
 
 # runs the csv_from_Excel function:
 if __name__ == '__main__':
-    args = parser.parse_args()
-
-    if not args.dataPath:
-        print("Data path \"" + args.dataPath + "\" does not exists.")
-        exit(1)
-
-    if not os.path.isdir(args.outputPath):
-        os.mkdir(args.outputPath)
-
-    for filename in os.listdir(args.dataPath):
-        if filename.endswith(".csv"):
-            import_csv_mongo(args.mongoUrl, args.mongoDatabase, args.outputPath)
-
-    if args.clear:
-        os.rmdir(args.outputPath)
+    import_csv_mongo(db_url, db_name, file_name)
