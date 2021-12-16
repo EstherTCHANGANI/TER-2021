0b8077cf2328d8c43d360345516a1c23ff04755e
Esther TCHANGANI <essossolim-esther.tchangani@etu.univ-cotedazur.fr>
2021-12-16 08:57:51 +0100

    handle cors headers

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const cors = require("cors");
 
 var indexRouter = require('./routes/index');
 var configRouter = require('./routes/config');
@@ -13,6 +14,7 @@ var eventsRouter = require('./routes/events');
 
 var app = express();
 
+app.use(cors())
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
